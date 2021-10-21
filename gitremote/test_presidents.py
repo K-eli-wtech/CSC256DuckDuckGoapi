82dcebd16e1d946556b3ae60d7cce9036fa84247
K-eli-wtech <kewhite3@my.waketech.edu>
2021-10-21 18:51:05 -0400

    Update test_presidents.py

--- gitremote/test_presidents.py
+++ gitremote/test_presidents.py
@@ -20,4 +20,6 @@ url_ddg = "https://api.duckduckgo.com"
 def test_ddg0(presidents):
     resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json")
     rsp_data = resp.json()
-    assert presidents in rsp_data["Text"]
+    rt = rsp_data["Text"]
+    text = rt[3]
+    assert presidents in text
