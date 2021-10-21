6235351b64b8493e6334d7db3ac38fc00c9c041b
K-eli-wtech <kewhite3@my.waketech.edu>
2021-10-21 16:09:07 -0400

    Modified for efficiency

--- gitremote/test_presidents.py
+++ gitremote/test_presidents.py
@@ -11,18 +11,12 @@ import requests
 url_ddg = "https://api.duckduckgo.com"
 
 
-@pytest.mark.parametrize("presidents", ["George Washington", "John Adams", "Thomas Jefferson", "James Madison",
-                                        "James Monroe", "John Quincy Adams", "Andrew Jackson", "Martin van Buren",
-                                        "William Henry Harrison", "John Tyler", "James Polk", "Zachary Taylor",
-                                        "Millard Fillmore", "Franklin Pierce", "James Buchanan", "Abraham Lincoln",
-                                        "Andrew Johnson", "Ulysses S. Grant", "Rutherford B. Hayes", "James Abram Garfield",
-                                        "Chester Alan Arthur", "Grover Cleveland", "Benjamin Harrison", "Grover Cleveland",
-                                        "William McKinley", "Theodore Roosevelt", "William Howard Taft", "Woodrow (Thomas) Wilson",
-                                        "Warren Gamaliel Harding", "Calvin (John) Coolidge", "Herbert Clark Hoover", "Franklin Delano Roosevelt",
-                                        "Harry S. Truman", "Dwight (David) Eisenhower", "John Fitzgerald Kennedy", "Lyndon Baines Johnson",
-                                        "Richard Milhouse Nixon", "Gerald Rudolph Ford", "Jimmy Carter", "Ronald Wilson Reagan",
-                                        "George Herbert Walker Bush", "William (Bill) Jefferson Clinton", "George Walker Bush", "Barack Hussein Obama",
-                                        "Donald Trump", "Joseph Robinette Biden Jr"])
+@pytest.mark.parametrize("presidents", ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren",
+                                        "Harrison", "Tyler", "Polk", "Taylor", "Fillmore", "Pierce", "Buchanan", "Lincoln",
+                                        "Johnson", "Grant", "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison", "Cleveland",
+                                        "McKinley", "Roosevelt", "Taft", "Wilson", "Harding", "Coolidge", "Hoover",
+                                        "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan",
+                                        "Bush", "Clinton", "Obama", "Trump", "Biden Jr"])
 def test_ddg0(presidents):
     resp = requests.get(url_ddg + "/?q=presidents of the united states, " + presidents + "&format=json")
     rsp_data = resp.json()
