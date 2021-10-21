703aed64f35ed8d811d5630a24f6e22793631148
K-eli-wtech <kewhite3@my.waketech.edu>
2021-10-21 16:25:20 -0400

    topics

--- gitremote/test_presidents.py
+++ gitremote/test_presidents.py
@@ -20,4 +20,4 @@ url_ddg = "https://api.duckduckgo.com"
 def test_ddg0(presidents):
     resp = requests.get(url_ddg + "/?q=presidents of the united states, " + presidents + "&format=json")
     rsp_data = resp.json()
-    assert presidents in rsp_data["Heading"]
+    assert presidents in rsp_data["RelatedTopics"]
