db8989cbf1b83dc216ffb3892eae449138d2407c
K-eli-wtech <kewhite3@my.waketech.edu>
2021-10-21 18:29:44 -0400

    please work

--- gitremote/test_presidents.py
+++ gitremote/test_presidents.py
@@ -18,7 +18,6 @@ url_ddg = "https://api.duckduckgo.com"
                                         "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan",
                                         "Bush", "Clinton", "Obama", "Trump", "Biden Jr"])
 def test_ddg0(presidents):
-    qdict = {"q": "presidents of the united states, " + presidents + "&format=json"}
-    resp = requests.get(url_ddg, qdict)
+    resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json")
     rsp_data = resp.json()
     assert presidents in rsp_data["RelatedTopics"]
